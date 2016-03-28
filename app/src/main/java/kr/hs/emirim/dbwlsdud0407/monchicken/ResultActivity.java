64963fd5acd5be85108b3202df2823aa2d047de4
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-03-28 20:52:54 +0900

    #4 lmplement TextView Result Activity

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/ResultActivity.java
@@ -2,15 +2,22 @@ package kr.hs.emirim.dbwlsdud0407.monchicken;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 /**
  * Created by appcreator08 on 2016. 3. 28..
  */
 public class ResultActivity extends ActionBarActivity {
+
+    TextView mResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult=(TextView)findViewById(R.id.result);
+        mResult.setText("결과");
 
     }
 }
