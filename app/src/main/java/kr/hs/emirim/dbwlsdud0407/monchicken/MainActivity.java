2b1fd05938e457f84558692109f0280097746c68
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-03-28 20:42:09 +0900

    #3 implement Activity

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/MainActivity.java
@@ -2,12 +2,27 @@ package kr.hs.emirim.dbwlsdud0407.monchicken;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText mName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText)findViewById(R.id.name);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mName.setText("");
     }
 }
