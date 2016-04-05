4f23b10fbacba4823d2dc502f0652187ad49d00e
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-04-05 19:05:00 +0900

    #4 add intent code

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/MainActivity.java
@@ -1,11 +1,14 @@
 package kr.hs.emirim.dbwlsdud0407.monchicken;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     EditText mName;
@@ -35,5 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Toast.makeText(this, "맛있는 치킨", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/monchicken/ResultActivity.java
@@ -14,7 +14,7 @@ public class ResultActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
 
         mResult=(TextView)findViewById(R.id.result);
         mResult.setText("결과");
